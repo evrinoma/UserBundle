219f58554a78fe2ce2686af4d756d7656d21aaa5
Nikolay Nikolaev <evrinoma@gmail.com>
2022-05-29 22:23:51 +0200

    fix Roles

--- src/Voter/RoleInterface.php
+++ src/Voter/RoleInterface.php
@@ -7,10 +7,5 @@ interface RoleInterface
 //region SECTION: Fields
     public const ROLE_USER        = 'ROLE_USER';
     public const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
-    public const ROLE_DEFAULT     = self::ROLE_USER;
-
-    public const ROLE_USER_       = 'ROLE_USER_';
-    public const ROLE_USER_CREATE = self::ROLE_USER_.'CREATE';
-    public const ROLE_USER_EDIT   = self::ROLE_USER_.'EDIT';
 //endregion Fields
 }
