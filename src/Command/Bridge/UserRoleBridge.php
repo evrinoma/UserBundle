760db9b786b27bc7b5070e61ba74587810b1d707
Nikolay Nikolaev <evrinoma@gmail.com>
2022-06-16 15:19:07 +0200

    little fix

--- src/Command/Bridge/UserRoleBridge.php
+++ src/Command/Bridge/UserRoleBridge.php
@@ -27,7 +27,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserRoleBridge implements BridgeInterface
 {
